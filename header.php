faec26c3170050368a5cd07d619887ef26f352d0
Web-projektitII <jukka.aula@contactorconnections.com>
2023-01-27 14:37:26 +0200

    title

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <head>
     <meta charset="utf-8">
-    <title>HTML-mallisivu</title>
+    <title>Neilikka-mallisovellus</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="site.css">
     <link rel="stylesheet" href="navbar.css">
