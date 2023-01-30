937a7865394a7f7131d6400ebf1f1320c709ab84
Web-projektitII <jukka.aula@contactorconnections.com>
2023-01-30 14:51:48 +0200

    lomake palvelimelle fetchillä

--- scripts.js
+++ scripts.js
@@ -3,3 +3,26 @@ const menutoggle = () => {
     if (x.className === "topnav") x.className += " responsive"; 
     else x.className = "topnav";
     }
+
+
+const tallennus = e => {
+console.log(e.target)    
+//let form = e.target    
+e.preventDefault();  
+let form = e.target  
+let formData = new FormData(form)
+formData.append('painike', 'Lähetä');
+
+fetch("yhteydenotto.php",{
+method:"POST",
+body: formData    
+})
+.then(response => response.json()) 
+.then(result => {
+    document.querySelector("#tulos").innerHTML = JSON.stringify(result)
+    console.log("result:",JSON.stringify(result))
+    }) 
+.catch(error => console.log(error)) 
+console.log("let",formData)   
+return false
+}    
--- yhteydenotto.php
+++ yhteydenotto.php
@@ -1,3 +1,12 @@
+<?php
+if (isset($_POST['painike'])){
+  /*foreach($_POST as $key => $value){
+    echo "$key:$value<br>";
+    }*/
+    echo json_encode($_POST);
+    exit;  
+}
+?>
 <!DOCTYPE html>
 <html lang="fi">
 <?php
@@ -9,22 +18,24 @@ include('header.php')
 <p>Voit ottaa meihin yhteyttä</p>
 <p class="nowrap">
 - puhelimitse <a href="myymalat.php">yksittäisiin myymälöihin</a><br>
-- sähköpostitse <a href="mailto:asiakaspalvelu@puutarhaliikeneilikka">asiakaspalvelu@puutarhaliikeneilikka.fi</a><br>
+- sähköpostitse <a href="mailto:asiakasespalvelu@puutarhaliikeneilikka">asiakaspalvelu@puutarhaliikeneilikka.fi</a><br>
 - alla olevalla lomakkeella<br>
 </p>
-<form id="yhteydenotto" onsubmit="tallennus()">
+<p id="tulos"></p>
+<form id="yhteydenotto" method="POST" novalidate onsubmit="tallennus(event)">
 <fieldset>
 <legend>Yhteydenottopyyntö</legend>
 <label class="label" for="nimi">Nimi</label>
-<input id="nimi" name="nimi" type="text" placeholder="nimi" autofocus required/><br>
+<input id="nimi" name="nimi" type="text" placeholder="nimi" minlength="2" maxlength="40" pattern="[a-zA-ZåäöÅÄÖ- ]+" autofocus required/><br>
 <label class="label" for="sposti">Sähköpostiosoite</label>
-<input id="posti" name="sposti" type="email" placeholder="sähköposti" required/><br>
+<input id="posti" name="sposti" type="email" placeholder="sähköposti" pattern="^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,4})+$"
+ required/><br>
 <label class="label" for="aihe">Aihe</label>
-<input id="aihe" name="aihe" type="text" placeholder="aihe" required/><br>
+<input id="aihe" name="aihe" type="text" placeholder="aihe" maxlength="40" required/><br>
 <label class="label label-textarea" for="viesti">Viesti</label>
-<textarea id="viesti" name="viesti" rows="4" cols="40" placeholder="Kirjoita viesti tähän." required></textarea><br>
+<textarea id="viesti" name="viesti" rows="4" cols="40" maxlength="256" placeholder="Kirjoita viesti tähän." required></textarea><br>
 <label class="label" for="tilaus">Haluan tilata Puutarhaliike Neilikan uutiskirjeen</label>
-<input id="tilaus" name="tilaus" type="checkbox"/><br>
+<input id="tilaus" name="tilaus" type="checkbox" value="1"/><br>
 <button type="submit" value="Lähetä" name="painike">Lähetä</button>
 </fieldset>
 </form>
